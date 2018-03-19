786f2338d0165ae7d500ffce6615383b2d05f06a
Raphael Ribeiro Tutui <31199384+raphatt@users.noreply.github.com>
2018-03-19 15:50:30 -0300

    Controlando os planos de energia do Windows com VB

---
+++ Power Plan Changer/Form1.Designer.vb
@@ -0,0 +1,79 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(84, 69)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Equilibrado"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(102, 12)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(84, 69)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Alto Desempenho"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(192, 12)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(84, 69)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Economia de Energia"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 93)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Power Plan Changer"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+End Class
---
+++ Power Plan Changer/Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+    Private Sub RadioButton1_Click(sender As Object, e As EventArgs)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim startinfo As New ProcessStartInfo("cmd", "/c powercfg /s 381b4222-f694-41f0-9685-ff5bb260df2e")
+        startinfo.WindowStyle = ProcessWindowStyle.Hidden
+        Process.Start(startinfo)
+        Close()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim startinfo As New ProcessStartInfo("cmd", "/c powercfg /s 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c")
+        startinfo.WindowStyle = ProcessWindowStyle.Hidden
+        Process.Start(startinfo)
+        Close()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim startinfo As New ProcessStartInfo("cmd", "/c powercfg /s a1841308-3541-4fab-bc81-f71556f20b4a")
+        startinfo.WindowStyle = ProcessWindowStyle.Hidden
+        Process.Start(startinfo)
+        Close()
+    End Sub
+End Class
